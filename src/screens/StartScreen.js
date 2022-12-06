474632fa1560661fba03535e6b0c6f5d74f3f70a
rypizani <rypizani@gmail.com>
2022-12-05 22:40:41 -0300

    V1

---
+++ src/screens/StartScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Paragraph from '../components/Paragraph';
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Template de Login</Header>
+      <Paragraph>Kit Start</Paragraph>
+      
+    </Background>
+  );
+}
